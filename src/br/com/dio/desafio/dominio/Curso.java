fcfd0ef13b82070ad1c3d7a2a5b7f130f630f90f
sara <sarathaiseg@gmail.com>
2023-01-07 19:42:16 -0300

    criando classes Bootcamp e Dev.

--- src/br/com/dio/desafio/dominio/Curso.java
+++ src/br/com/dio/desafio/dominio/Curso.java
@@ -1,29 +1,13 @@
 package br.com.dio.desafio.dominio;
 
-public class Curso {
+public class Curso extends Conteudo {
 
-    private String titulo;
-    private String descricao;
+
     private int cargoHoraria;
 
     public Curso() {
     }
 
-    public String getTitulo() {
-        return titulo;
-    }
-
-    public void setTitulo(String titulo) {
-        this.titulo = titulo;
-    }
-
-    public String getDescricao() {
-        return descricao;
-    }
-
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
-    }
 
     public int getCargoHoraria() {
         return cargoHoraria;
@@ -34,10 +18,15 @@ public class Curso {
     }
 
     @Override
+    public double calcularXp() {
+        return XP_PADRAO * this.cargoHoraria; // regra de negócio
+    }
+
+    @Override
     public String toString() {
         return "Curso{" +
-                "titulo='" + titulo + '\'' +
-                ", descricao='" + descricao + '\'' +
+                "titulo='" + getTitulo() + '\'' +
+                ", descricao='" + getDescricao() + '\'' +
                 ", cargoHoraria=" + cargoHoraria +
                 '}';
     }
